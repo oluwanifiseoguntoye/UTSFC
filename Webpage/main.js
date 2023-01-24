d143d6395f3934542711383aa0eee8cef8c3fc0a
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-01-24 13:07:42 -0500

    Navbar toggler fix

--- Webpage/main.js
+++ Webpage/main.js
@@ -1,3 +1,12 @@
 $(function(){
     $('.carousel').carousel()
 })
+
+let button = document.querySelector('.navbar-toggler');
+let menu = document.querySelector('.collapse');
+
+button.addEventListener('click', function() {
+  menu.classList.toggle('show');
+});
+
+
